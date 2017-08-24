7cbb9e12bffd8aa2624e5f6aea8fda84f3ffb04b
Shane Isbell <shane.isbell@gmail.com>
2017-08-23 21:47:28 -0700

    Remove deprecated CLI. fixes #12

--- oulipo-machine-server/src/main/java/org/oulipo/machine/server/Main.java
+++ oulipo-machine-server/src/main/java/org/oulipo/machine/server/Main.java
@@ -18,7 +18,6 @@ package org.oulipo.machine.server;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
@@ -26,58 +25,29 @@ import org.apache.commons.cli.PosixParser;
 public class Main {
 
 	private static Options addOptions() {
-        Options options = new Options();
-        Option helpOption = Option.builder("h").longOpt("help")
-                .desc("Display help").build();
-        Option databaseOption = Option.builder("db").longOpt("databaseDir")
-                .desc("Directory of database").build();
-        Option portOption = Option.builder("p").longOpt("port")
-                .desc("Server port").build();
-        
-        OptionBuilder.withLongOpt("summary");
-		OptionBuilder
-                .withDescription("Output as summary statics");
-		Option summaryOption = OptionBuilder
-                .create("s");
-        OptionBuilder.withLongOpt("output");
-		OptionBuilder
-                .withDescription("Log output file");
-		Option outputOption = OptionBuilder
-                .create("o");
-        OptionBuilder.withLongOpt("raw");
-		OptionBuilder
-                .withDescription("Output as raw delimited format");
-		Option rawOption = OptionBuilder
-                .create("r");
+		Options options = new Options();
+		Option helpOption = Option.builder("h").longOpt("help").desc("Display help").build();
+		Option databaseOption = Option.builder("db").longOpt("databaseDir").desc("Directory of database").build();
+		Option portOption = Option.builder("p").longOpt("port").desc("Server port").build();
 
-        
-        OptionBuilder.hasOptionalArg();
-		OptionBuilder.withLongOpt("config");
-		OptionBuilder
-                .withDescription("config.json file for defining log events");
-		Option configOption = OptionBuilder
-                .create("c");
+		return options.addOption(portOption).addOption(databaseOption).addOption(helpOption);
+	}
 
-        return options.addOption(portOption).addOption(databaseOption).addOption(helpOption).addOption(rawOption)
-                .addOption(outputOption).addOption(configOption);
-    }
-	
-    private static void exit(String message) {
-        System.out.println("\r\n" + message + "\r\n");
-      //  printHelp();
-        System.exit(-1);
-    }
-    
+	private static void exit(String message) {
+		System.out.println("\r\n" + message + "\r\n");
+		// printHelp();
+		System.exit(-1);
+	}
+
 	public static void main(String[] args) {
-		  CommandLineParser parser = new PosixParser();
-	        CommandLine commandLine = null;
+		CommandLineParser parser = new PosixParser();
+		CommandLine commandLine = null;
 
-	        try {
-	            commandLine = parser.parse(addOptions(), args);
-	        } catch (ParseException ex) {
-	            exit("Invalid command arguments");
-	        }
-	        
-	        
+		try {
+			commandLine = parser.parse(addOptions(), args);
+		} catch (ParseException ex) {
+			exit("Invalid command arguments");
+		}
+
 	}
 }
